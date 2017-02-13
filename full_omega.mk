dc6b998f5a1ed2be29dd78dccf2c668864e5a3b5
Goayandi <r.borutha@gmail.com>
2017-02-13 13:28:27 +0100

    Fix path xiaomi/nikel to xiaomi/omega

--- full_omega.mk
+++ full_omega.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/xiaomi/nikel/omega.mk)
+$(call inherit-product, device/xiaomi/omega/omega.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME :=  omega
